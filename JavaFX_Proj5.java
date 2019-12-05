40616dfaf0a40a3ff6186377eb5059bb17186c6d
Valdus145 <55852104+Valdus145@users.noreply.github.com>
2019-12-05 11:04:07 -0600

    Fixed add station loop bug

--- JavaFX_Proj5.java
+++ JavaFX_Proj5.java
@@ -191,7 +191,6 @@ public class JavaFX_Proj5 extends Application {
             				write.println(stationToAdd);
             				write.close();
             				mesonet.add(stationToAdd);
-            				dropbox.getItems().add(stationToAdd);
             			} catch (IOException e) {
             				System.out.println(e);
             				e.printStackTrace();
